de398075357ef9930e62085554d1f51db1b6543a
tkerns1965 <tony.kerns.65@gmail.com>
2018-11-20 14:07:44 -0500

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -6,6 +6,6 @@ sleep 5
 startfluxbox &
 sleep 5
 x11vnc -display $DISPLAY -rfbport 5901 -usepw &
-sleep 5
-/docker/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
+# sleep 5
+# /docker/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
 wait $!
