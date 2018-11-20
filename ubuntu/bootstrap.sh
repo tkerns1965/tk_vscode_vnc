3ba1de3f2ae2ad156259e083f1342cae4414903a
tkerns1965 <tony.kerns.65@gmail.com>
2018-11-20 13:57:01 -0500

    Create bootstrap.sh

---
+++ ubuntu/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p "$HOME/.vnc"
+x11vnc -storepasswd "2hard4u" "$HOME/.vnc/passwd"
+Xvfb $DISPLAY -screen 0 1366x768x24 &
+sleep 5
+startfluxbox &
+sleep 5
+x11vnc -display $DISPLAY -rfbport 5901 -usepw &
+sleep 5
+/docker/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
+wait $!
