0d749334d2266825e441808bfc07cb3f2173319c
tkerns1965 <tony.kerns.65@gmail.com>
2018-11-21 09:40:45 -0500

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,7 +3,7 @@ mkdir -p "$HOME_DIR/.vnc"
 x11vnc -storepasswd "2hard4u" "$HOME_DIR/.vnc/passwd"
 Xvfb $DISPLAY -screen 0 1280x720x24 &
 sleep 5
-startfluxbox &
+gosu root startfluxbox &
 sleep 5
 x11vnc -display $DISPLAY -rfbport 5901 -usepw -forever -q &
 # sleep 5
