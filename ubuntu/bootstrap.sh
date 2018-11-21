54ed411f1abb552d296e29a438e910f7195a67c9
tkerns1965 <tony.kerns.65@gmail.com>
2018-11-21 09:49:03 -0500

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,9 +3,9 @@ mkdir -p "$HOME_DIR/.vnc"
 x11vnc -storepasswd "2hard4u" "$HOME_DIR/.vnc/passwd"
 Xvfb $DISPLAY -screen 0 1280x720x24 &
 sleep 5
-gosu root startfluxbox &
+startfluxbox &
 sleep 5
-x11vnc -display $DISPLAY -rfbport 5901 -usepw -forever -q &
+gosu root x11vnc -display $DISPLAY -rfbport 5901 -usepw -forever -q &
 # sleep 5
 # /docker/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
 wait $!
